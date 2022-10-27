214287ad32deb53ff468c1fe8e84b35da551617c
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-10-27 20:29:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,25 +38,20 @@ def ids(appd):
         titulo = "Current weather data"
         titulo2 = "Current weather data"
 
-        try:
-            connection = psycopg2.connect(
-                host="192.168.50.45",
-                database="appweb-db",
-                user="appwebuser",
-                password="appwebpass"
-            )
-            print("Conexión realizada")
-            cursor=connection.cursor()        
-            cursor.execute("select * from users")
-            row=cursor.fetchone()
-            for rows in row:
-                listadatos.append(rows)            
+        connection = psycopg2.connect(
+            host="192.168.50.45",
+            database="appweb-db",
+            user="appwebuser",
+            password="appwebpass"
+        )
+        print("Conexión realizada")
+        cursor=connection.cursor()        
+        cursor.execute("select * from users")
+        row=cursor.fetchone()
+        for rows in row:
+            listadatos.append(rows)            
             #print(row)
-        except Exception as ex:
-            print(ex)
-        finally:
-            connection.close()
-            print("Conexión finalizada.")
+
 
         #for rows in postgres():
         #     listadatos.append(rows)
