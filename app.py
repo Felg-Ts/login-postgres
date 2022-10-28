4886179ba55128063a4daee827d62e00d0db4cca
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-10-28 20:10:11 +0200

    error

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def ids(appd):
             )
             print("Conexión realizada")
             cursor=connection.cursor()        
-            cursor.execute(f"select * from users where username='{usernameform}' and password='{passform}'")
+            cursor.execute(f"select * from users where username='{usernameform}' and password=md5('{passform}')")
             print(cursor.execute(f"select * from users where username='{usernameform}' and password='{passform}'"))
             row=cursor.fetchall()
             print(row)
