58e4f9e9da1602fc6367965bd1be11f8e40ebaf1
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-10-28 19:39:44 +0200

    validación login

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #Librerías
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import psycopg2
 
 app = Flask(__name__)	
@@ -16,35 +16,62 @@ def inicio():
 def ids(appd):
 
     if appd == "log":
-
+        
         listadatos = []
-
-        rutaid = "/dma/"
+        usernameform = request.form.get("formusername")
+        passform = request.form.get("formpass")
 
         try:
             connection = psycopg2.connect(
-                host="192.168.50.45",
-                database="appweb-db",
-                user="appwebuser",
-                password="appwebpass"
+            host="192.168.50.45",
+            database="appweb-db",
+            user="appwebuser",
+            password="appwebpass"
             )
             print("Conexión realizada")
             cursor=connection.cursor()        
-            cursor.execute("select username,nombre,horas_semanales,profesor from users,modulos,matriculaciones where matriculaciones.alumnos=users.id and matriculaciones.modulos=modulos.nombre and users.username='usuario2'")
+            cursor.execute(f"select * from users where username={usernameform} and password={passform}")
             row=cursor.fetchall()
             for rows in row:
                 listadatos.append(rows)
-            nombre= listadatos[0][0]
         except Exception as ex:
             print(ex)
         finally:
             connection.close()
             print("Conexión finalizada.")
-
+    
         if len(listadatos) == 0:
-            return render_template("error404.html",titulo="Error404",titulo2="Error404",errormesaje="Los caracteres introducidos no coinciden con ningún nombre. Recuerde que la primera letra de la ciudad tiene que ser en mayúsculas",urlform="/forms/dma")
+                return render_template("error404.html",titulo="Error404",titulo2="Error404",errormesaje="Los caracteres introducidos no coinciden con ningún nombre. Recuerde que la primera letra de la ciudad tiene que ser en mayúsculas",urlform="/forms/dma")
 
-        return render_template("site.html",titulo="site",listadatos=listadatos,rutaid=rutaid,nombre=nombre)
+        else:
+            listadatos = []
+
+            rutaid = "/dma/"
+
+            try:
+                connection = psycopg2.connect(
+                 host="192.168.50.45",
+                 database="appweb-db",
+                 user="appwebuser",
+                 password="appwebpass"
+                )
+                print("Conexión realizada")
+                cursor=connection.cursor()        
+                cursor.execute(f"select username,nombre,horas_semanales,profesor from users,modulos,matriculaciones where matriculaciones.alumnos=users.id and matriculaciones.modulos=modulos.nombre and users.username={usernameform}")
+                row=cursor.fetchall()
+                for rows in row:
+                    listadatos.append(rows)
+                nombre= listadatos[0][0]
+            except Exception as ex:
+                print(ex)
+            finally:
+                connection.close()
+                print("Conexión finalizada.")
+
+            if len(listadatos) == 0:
+                return render_template("error404.html",titulo="Error404",titulo2="Error404",errormesaje="Los caracteres introducidos no coinciden con ningún nombre. Recuerde que la primera letra de la ciudad tiene que ser en mayúsculas",urlform="/forms/dma")
+
+            return render_template("site.html",titulo="site",listadatos=listadatos,rutaid=rutaid,nombre=nombre)
 
 if __name__ == '__main__':
     app.run("0.0.0.0",5000,debug=True)
