498f437db59bad1beeac0f0b998b9f66367bb435
Felg-Ts <el-feli-rubio1996@hotmail.com>
2022-11-23 10:13:44 +0100

    cambio

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #Librerías
 from flask import Flask,render_template,request
 import psycopg2
+from psycopg2 import sql
 
 app = Flask(__name__)	
 
@@ -30,9 +31,7 @@ def ids(appd):
             )
             print("Conexión realizada")
             cursor=connection.cursor()        
-            #cursor.execute(f"select username from users where username='{usernameform}' and password=md5('{passform}')")
-            #cursor.execute(f"select username from users where username= %(usernameform)s and password=md5(%(passform)s)",'usuario'}))
-            cursor.execute("select username from users where username = %(username)s and password=md5(%(password)s)", {'username': usernameform, 'password': passform})
+            cursor.execute(f"select username from users where username='{usernameform}' and password=md5('{passform}')")
             row=cursor.fetchall()
             for rows in row:
                 listadatos.append(rows)
